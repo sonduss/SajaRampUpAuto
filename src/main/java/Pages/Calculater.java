39f375637fcbca15c2aaa128af6a83a1c2c26742
sajasalahat <sajasalahat1@icloid.com>
2023-12-08 15:22:32 +0300

    Junit Task

---
+++ src/main/java/Pages/Calculater.java
@@ -0,0 +1,23 @@
+package Pages;
+
+public class Calculater {
+
+    public float AddProcess(float x, float y) {
+        return x + y;
+    }
+
+
+    public float SubtractProcess(float x, float y) {
+        return x - y;
+    }
+
+    public float MultiplyProcess(float x, float y) {
+        return x * y;
+    }
+
+    public float DivideProcess(float x, float y) {
+        return (x / y);
+    }
+
+
+}
---
+++ src/main/java/testing/CalculaterProcess.java
@@ -0,0 +1,55 @@
+package testing;
+
+import Pages.*;
+import org.testng.annotations.*;
+
+@Test
+public class CalculaterProcess {
+    Calculater Calculater_;
+
+    @BeforeTest
+    public void SetUp() {
+        Calculater_ = new Calculater();
+        System.out.println("Started testing the great calculator app");
+    }
+
+    @Test
+    public void validateAddProcess() {
+        Calculater_.AddProcess(4, 6);
+    }
+
+    @Test
+    public void validateSubtractProcess() {
+        Calculater_.SubtractProcess(4, 6);
+    }
+
+    @Test
+    //@DisplayName("Multiply two numbers")
+    public void validateMultiplyProcess() {
+        Calculater_.MultiplyProcess(4, 6);
+    }
+
+
+    @Test
+    public void validateDivideProcess() {
+        Calculater_.DivideProcess(4, 6);
+    }
+
+    @AfterTest
+    public void Finislize() {
+        System.out.println("Finished testing the great calculator app");
+    }
+
+    @BeforeClass
+    public void BeforeClass_() {
+        System.out.println("Starting a boring test");
+    }
+
+    @AfterClass
+    public void AfterClass_() {
+        System.out.println("Finished a boring test");
+    }
+
+}
+
+
