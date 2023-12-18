c9362a597465ccfd702c606a3a1d52fecf003257
sajasalahat <sajasalahat1@icloid.com>
2023-12-18 18:07:46 +0300

    Hi, this is the first commit for Google Search .

---
+++ src/GoogleSearch/GoogleSearch.java
@@ -0,0 +1,23 @@
+package src.GoogleSearch;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+public class GoogleSearch {
+
+    private WebElement Element = null;
+
+    public WebElement TestBox_Search(WebDriver Driver) {
+        WebElement Element = Driver.findElement(By.name("q"));
+       Element.sendKeys("Selenium Tutorials");
+        //Element.click();
+        return Element;
+    }
+
+    public WebElement ButtonSearch(WebDriver Driver) {
+        Element = Driver.findElement(By.name("q"));
+        Element.sendKeys(Keys.RETURN);
+
+        return Element;
+    }
+}
---
+++ src/GoogleSearch/TitleSearch.java
@@ -0,0 +1,14 @@
+package src.GoogleSearch;
+import org.testng.Assert;
+import org.openqa.selenium.WebDriver;
+
+public class TitleSearch {
+
+   public void AssertionTitleResult_(WebDriver Driver) {
+       String pageTitle = Driver.getTitle();
+       String expectedTitle = "Selenium Tutorials";
+       Assert.assertEquals("Selenium Tutorials", expectedTitle, pageTitle);
+       //System.out.println(" Validation Title done");
+   }
+
+   }
